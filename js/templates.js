1745929ff54efc4c9ab8d82ec10a293944e25506
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2025-01-28 01:05:34 +0100

    site redesign

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,122 @@
-!function(){var a=Handlebars.template,e=Handlebars.templates=Handlebars.templates||{};e.albumDetails=a({1:function(a,e,l,t,n){var i,s=this.lambda,r=this.escapeExpression,c="function",u=e.helperMissing;return'		<div id="'+r(s(t&&t.index,a))+'" style="background-image: url(\'/pictures/'+r(s(null!=n[1]?n[1].title:n[1],a))+"/"+r((i=null!=(i=e.title||(null!=a?a.title:a))?i:u,typeof i===c?i.call(a,{name:"title",hash:{},data:t}):i))+"')\"></div>\n"},compiler:[6,">= 2.0.0-beta.1"],main:function(a,e,l,t,n){var i,s,r="function",c=e.helperMissing,u=this.escapeExpression,o='<div class="albumDescription">\n	<h1 style="text-align: center; width: 100%">'+u((s=null!=(s=e.friendlyTitle||(null!=a?a.friendlyTitle:a))?s:c,typeof s===r?s.call(a,{name:"friendlyTitle",hash:{},data:t}):s))+"</h1>\n	"+u((s=null!=(s=e.description||(null!=a?a.description:a))?s:c,typeof s===r?s.call(a,{name:"description",hash:{},data:t}):s))+'\n</div>\n\n<div class="photoList">\n';return i=e.each.call(a,null!=a?a.pictures:a,{name:"each",hash:{},fn:this.program(1,t,n),inverse:this.noop,data:t}),null!=i&&(o+=i),o+"</div>"},useData:!0,useDepths:!0}),e.albums=a({1:function(a,e,l,t){var n,i=this.lambda,s=this.escapeExpression,r="function",c=e.helperMissing;return'		<div id="'+s(i(t&&t.index,a))+'">\n			<div class="title">'+s((n=null!=(n=e.friendlyTitle||(null!=a?a.friendlyTitle:a))?n:c,typeof n===r?n.call(a,{name:"friendlyTitle",hash:{},data:t}):n))+'</div>\n			<div class="albumDate">'+s((n=null!=(n=e.date||(null!=a?a.date:a))?n:c,typeof n===r?n.call(a,{name:"date",hash:{},data:t}):n))+'</div>\n			<div class="back" style="background-image: url(\'/pictures/'+s((n=null!=(n=e.title||(null!=a?a.title:a))?n:c,typeof n===r?n.call(a,{name:"title",hash:{},data:t}):n))+"/"+s((n=null!=(n=e.backImageUrl||(null!=a?a.backImageUrl:a))?n:c,typeof n===r?n.call(a,{name:"backImageUrl",hash:{},data:t}):n))+"')\"></div>\n		</div>\n"},compiler:[6,">= 2.0.0-beta.1"],main:function(a,e,l,t){var n,i='<div class="albumList">\n';return n=e.each.call(a,a,{name:"each",hash:{},fn:this.program(1,t),inverse:this.noop,data:t}),null!=n&&(i+=n),i+"</div>"},useData:!0}),e.contact=a({compiler:[6,">= 2.0.0-beta.1"],main:function(a,e,l,t){var n,i="function",s=e.helperMissing,r=this.escapeExpression;return'<div class="contact">\n	e-mail: <a href="mailto:sterre_rooijakers@hotmail.com">sterre_rooijakkers@hotmail.com</a>\n</div>\n\n<div class="lastPhotoContact" style="background-image: url(\''+r((n=null!=(n=e.currentImage||(null!=a?a.currentImage:a))?n:s,typeof n===i?n.call(a,{name:"currentImage",hash:{},data:t}):n))+"');\">\n\n</div>"},useData:!0}),e.index=a({compiler:[6,">= 2.0.0-beta.1"],main:function(){return""},useData:!0}),e.photos=a({1:function(a,e,l,t,n){var i,s=this.lambda,r=this.escapeExpression,c="function",u=e.helperMissing;return'		<div><img data-lazy="/pictures/'+r(s(null!=n[1]?n[1].title:n[1],a))+"/"+r((i=null!=(i=e.title||(null!=a?a.title:a))?i:u,typeof i===c?i.call(a,{name:"title",hash:{},data:t}):i))+'"/></div>\n'},compiler:[6,">= 2.0.0-beta.1"],main:function(a,e,l,t,n){var i,s='<div class="photoSlider">\n';return i=e.each.call(a,null!=a?a.pictures:a,{name:"each",hash:{},fn:this.program(1,t,n),inverse:this.noop,data:t}),null!=i&&(s+=i),s+"</div>"},useData:!0,useDepths:!0})}();
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['albumDetails'] = template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "		<div id=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"index") || (data && lookupProperty(data,"index"))) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"index","hash":{},"data":data,"loc":{"start":{"line":8,"column":11},"end":{"line":8,"column":21}}}) : helper)))
+    + "\" style=\"background-image: url('/pictures/"
+    + alias4(container.lambda((depths[1] != null ? lookupProperty(depths[1],"title") : depths[1]), depth0))
+    + "/"
+    + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":8,"column":76},"end":{"line":8,"column":85}}}) : helper)))
+    + "')\"></div>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"albumDescription\">\n	<h1 style=\"text-align: center; width: 100%\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"friendlyTitle") || (depth0 != null ? lookupProperty(depth0,"friendlyTitle") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"friendlyTitle","hash":{},"data":data,"loc":{"start":{"line":2,"column":45},"end":{"line":2,"column":62}}}) : helper)))
+    + "</h1>\n	"
+    + alias4(((helper = (helper = lookupProperty(helpers,"description") || (depth0 != null ? lookupProperty(depth0,"description") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data,"loc":{"start":{"line":3,"column":1},"end":{"line":3,"column":16}}}) : helper)))
+    + "\n</div>\n\n<div class=\"photoList\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"pictures") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":7,"column":1},"end":{"line":9,"column":10}}})) != null ? stack1 : "")
+    + "</div>";
+},"useData":true,"useDepths":true});
+templates['albums'] = template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "		<div id=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"index") || (data && lookupProperty(data,"index"))) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"index","hash":{},"data":data,"loc":{"start":{"line":3,"column":11},"end":{"line":3,"column":21}}}) : helper)))
+    + "\" class=\"albumBlock\">\n			<div class=\"title\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"friendlyTitle") || (depth0 != null ? lookupProperty(depth0,"friendlyTitle") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"friendlyTitle","hash":{},"data":data,"loc":{"start":{"line":4,"column":22},"end":{"line":4,"column":39}}}) : helper)))
+    + "</div>\n			<div class=\"description\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"description") || (depth0 != null ? lookupProperty(depth0,"description") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data,"loc":{"start":{"line":5,"column":28},"end":{"line":5,"column":43}}}) : helper)))
+    + "</div>\n			<div class=\"albumDate\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"date") || (depth0 != null ? lookupProperty(depth0,"date") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data,"loc":{"start":{"line":6,"column":26},"end":{"line":6,"column":34}}}) : helper)))
+    + "</div>\n			<div class=\"pictures\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"pictures") : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":8,"column":4},"end":{"line":12,"column":13}}})) != null ? stack1 : "")
+    + "			</div>\n		</div>\n";
+},"2":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var helper, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), alias4=container.hooks.helperMissing, alias5="function", lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "					<a href=\"/pictures/"
+    + alias2(alias1((depths[1] != null ? lookupProperty(depths[1],"title") : depths[1]), depth0))
+    + "/"
+    + alias2(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":9,"column":37},"end":{"line":9,"column":46}}}) : helper)))
+    + "\" target=\"_blank\">\n						<img src=\"/pictures/"
+    + alias2(alias1((depths[1] != null ? lookupProperty(depths[1],"title") : depths[1]), depth0))
+    + "/"
+    + alias2(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":10,"column":39},"end":{"line":10,"column":48}}}) : helper)))
+    + "\"/>\n					</a>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"albumList\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":15,"column":10}}})) != null ? stack1 : "")
+    + "</div>\n";
+},"useData":true,"useDepths":true});
+templates['contact'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"contact\">\n	e-mail: <a href=\"mailto:sterre_rooijakers@hotmail.com\">sterre_rooijakkers@hotmail.com</a>\n</div>\n\n<div class=\"lastPhotoContact\" style=\"background-image: url('"
+    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"currentImage") || (depth0 != null ? lookupProperty(depth0,"currentImage") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"currentImage","hash":{},"data":data,"loc":{"start":{"line":5,"column":60},"end":{"line":5,"column":76}}}) : helper)))
+    + "');\">\n\n</div>";
+},"useData":true});
+templates['index'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+templates['photos'] = template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var helper, alias1=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "		<div><img data-lazy=\"/pictures/"
+    + alias1(container.lambda((depths[1] != null ? lookupProperty(depths[1],"title") : depths[1]), depth0))
+    + "/"
+    + alias1(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"title","hash":{},"data":data,"loc":{"start":{"line":3,"column":46},"end":{"line":3,"column":55}}}) : helper)))
+    + "\"/></div>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"photoSlider\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"pictures") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":4,"column":10}}})) != null ? stack1 : "")
+    + "</div>";
+},"useData":true,"useDepths":true});
+})();
